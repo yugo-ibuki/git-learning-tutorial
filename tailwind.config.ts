179612511a95423a5f68cb1369c0adf4e45e312c
yugo-ibuki <y.ibuki91@gmail.com>
2024-11-18 01:21:41 +0900

    Add custom CSS variables and clean up Tailwind config
    Added custom CSS variables for theming and apply styles for light and dark modes. Removed default font family configuration from Tailwind to simplify the setup.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import { type Config } from "tailwindcss"
-import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
 	darkMode: ["class"],
@@ -55,9 +54,6 @@ export default {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
-			},
-			fontFamily: {
-				sans: ["var(--font-sans)", ...fontFamily.sans],
 			},
 		},
 	},
